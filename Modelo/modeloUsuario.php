ac99edc3dbe69d6b9ffcf5d2883dcd182fa11ce1
KatyaCP <testmail23571113@gmail.com>
2021-12-14 07:06:36 -0500

    modificacion funcion consultar

--- Modelo/modeloUsuario.php
+++ Modelo/modeloUsuario.php
@@ -1,12 +1,11 @@
 <?php
-    class modeloUsuario
+class modeloUsuario
     {   
         public function validarConsulta($conexion,$user,$pass)
         {
-            
-            $idtrabajo=1;
-            $sentencia = $conexion->prepare("SELECT * FROM Tusuario where IdTrabaj=? and usuario=? AND contrasena=aes_encrypt(?,'admin') ");
-            $sentencia->bindParam(1,$idtrabajo);
+            $idCargo=1;
+            $sentencia = $conexion->prepare("SELECT * FROM Tusuario tu INNER JOIN Ttrabajador tr ON tu.IdTrabaj=tr.IdTrabaj where tr.IdCargo=? and tu.usuario=? AND tu.contrasena=aes_encrypt(?,'admin') ");
+            $sentencia->bindParam(1,$idCargo);
             $sentencia->bindParam(2,$user);  
             $sentencia->bindParam(3,$pass);
             $sentencia->execute();
@@ -16,5 +15,4 @@
             return 0;
         }
     }
-
 ?>
